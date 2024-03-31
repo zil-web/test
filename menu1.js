49d46617612a73e1ddf2faacc2dcf198b3d7d0dd
zil-web <126854462+zil-web@users.noreply.github.com>
2024-03-31 22:11:53 +0700

    first commit

---
+++ menu1.js
@@ -0,0 +1,17 @@
+function showAlert(){
+    alert(Date(),tampil(),show())
+    Date()
+
+    let tanggal = new Date();
+    document.getElementById("hasil").innerHTML = tanggal;
+
+    function tampil() {
+        alert("Harith yang merupakan mage kini berusia 15 tahun, pada saat itu, ia sudah tidak tahan dengan kebiasaan bangsanya yang begitu saja. Kemudian Harith akan memutuskan untuk meninggalkan Enchanted Forest dan kembali ke dunia luar untuk membantu manusia dalam mengusir para Orc. ")
+    } 
+
+    function show(){
+        alert("Dalam Ras Ancient, terdapat seseorang yang bernama Harith, ia sangat berbakat dan bijaksana sejak ia kecil. Dengan kemampuannya, Harith berhasil menjadi mage terkuat dari bangsanya.")
+
+        
+    }
+}
